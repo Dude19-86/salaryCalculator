5adb445452b03ba256e92192d65de03d978b21c3
Dude19-86 <88909475+Dude19-86@users.noreply.github.com>
2023-07-27 12:31:01 +0300

    2salary

--- lib/salary_calculator.dart
+++ lib/salary_calculator.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:salary_calculator/text_field_salary_calculator.dart';
-
 
 class SalaryCalculator extends StatefulWidget {
   const SalaryCalculator({super.key});
@@ -14,17 +12,129 @@ class _SalaryCalculatorState extends State<SalaryCalculator> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(32),
-      child: const Row(
+      child: Row(
         children: [
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextFieldSalary(),
-                TextFieldSalary(),
-                TextFieldSalary(),
-                TextFieldSalary(),
-                Padding(
+                Row(
+                  children: [
+                    const SizedBox(
+                      child: Text('0'),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: TextField(
+                          // controller: this.controller,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Day Hours',
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add, color: Colors.indigo),
+                    ),
+                    const SizedBox(
+                      child: Text('Amount'),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      child: Text('0'),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: TextField(
+                          // controller: this.controller,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Night Hours',
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add, color: Colors.indigo),
+                    ),
+                    const SizedBox(
+                      child: Text('Amount'),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      child: Text('0'),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: TextField(
+                          // controller: this.controller,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Part Time Job',
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add, color: Colors.indigo),
+                    ),
+                    const SizedBox(
+                      child: Text('Amount'),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      child: Text('0'),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: SizedBox(
+                        width: 100,
+                        height: 30,
+                        child: TextField(
+                          // controller: this.controller,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Day Hours',
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add, color: Colors.indigo),
+                    ),
+                    const SizedBox(
+                      child: Text('Amount'),
+                    ),
+                  ],
+                ),
+                const Padding(
                   padding: EdgeInsets.all(50),
                   child: SizedBox(
                     child: Row(
